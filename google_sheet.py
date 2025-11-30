7939ed4243fca602231411e975060eabb24baf32
Vaibhav Hedaoo <vaibhavhedaoo12@gmail.com>
2025-11-30 22:09:05 +0530

    Update google_sheet.py

--- google_sheet.py
+++ google_sheet.py
@@ -1,1 +1,25 @@
+import gspread
+import json
+import os
+from oauth2client.service_account import ServiceAccountCredentials
+
+def get_next_row():
+    credentials_json = os.getenv("GOOGLE_SHEET_CREDENTIALS")
+    sheet_id = os.getenv("SHEET_ID")
+
+    creds_dict = json.loads(credentials_json)
+
+    scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
+    creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
+
+    client = gspread.authorize(creds)
+    sheet = client.open_by_key(sheet_id).worksheet("Master")
+
+    rows = sheet.get_all_records()
+
+    for index, row in enumerate(rows, start=2):  # starts row indexing at 2 to skip header
+        if row['status'].lower() == 'pending':
+            return row, index
+
+    return None, None
 
