86b6b9adcc4d367467d221fc1fd9a6cb6f90dbe0
Vaibhav Hedaoo <vaibhavhedaoo12@gmail.com>
2025-12-02 09:25:05 +0530

    Update create_video.py

--- create_video.py
+++ create_video.py
@@ -1,1 +1,10 @@
+from moviepy.editor import *
+import os
 
+def create_video(bg_path, voice_path, output="final.mp4"):
+    audio = AudioFileClip(voice_path)
+    clip = VideoFileClip(bg_path).subclip(0, audio.duration)
+    clip = clip.resize((1080, 1920))
+    final = clip.set_audio(audio)
+    final.write_videofile(output, fps=30)
+    return output
