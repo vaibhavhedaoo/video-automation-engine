f41fb4d2b3cc0ed0aceadf8ba3343856853812fe
Vaibhav Hedaoo <vaibhavhedaoo12@gmail.com>
2025-12-02 11:23:41 +0530

    Update generate_voice.py

--- generate_voice.py
+++ generate_voice.py
@@ -3,4 +3,8 @@ from gtts import gTTS
 def create_voice(text, output_path="voice.mp3", lang="en"):
     tts = gTTS(text=text, lang=lang)
     tts.save(output_path)
+
+    with open("voice.txt", "w") as f:
+        f.write(text)
+
     return output_path
